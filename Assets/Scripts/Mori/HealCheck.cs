1823f781f2348b1b0f168afeb5a4e851eb15a877
MorrinoahJin <71023609+MorrinoahJin@users.noreply.github.com>
2021-11-21 01:35:53 +0900

    힐 좀비 만드는 중
    enemy씬의 enemy1에 임시로 힐좀비를 만들어 둠
    내일 힐좀비를 다 만들고 새로운 프리펩으로 만들기
    모든 enemy에 Istrigger프리펩 만들기
    출돌체크하는 놈 태그 enemyheal에서 enemy로 바꾸기

---
+++ Assets/Scripts/Mori/HealCheck.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealCheck : MonoBehaviour
+{
+    float time;
+    bool healcheck;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;   
+        healcheck = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(healcheck == true)
+        {
+            time += Time.deltaTime;
+            if(time >= 2.5)
+            {
+                Debug.Log("회복 시키는 중");
+                time = 0;
+            }
+        }
+        Debug.Log(healcheck);
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "EnemyHeal")
+        {
+            healcheck = true;
+        }
+        else
+            healcheck = false;
+    }
+}
