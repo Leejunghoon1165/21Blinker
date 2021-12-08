f5718a0eb81b7603be14728a2d4caede64a1efe5
안세림 <dkstpfla11@gmail.com>
2021-12-09 02:44:37 +0900

    SET
    SET

--- Assets/Scripts/Lee/Player/Camera.cs
+++ Assets/Scripts/Lee/Player/Camera.cs
@@ -8,9 +8,22 @@ public class Camera : MonoBehaviour
     public Vector3 offset;
     internal CameraClearFlags clearFlags;
 
+    
+    Renderer ObstacleRenderer;  // 레이 검출된 건물의 렌더링
     void Update()
     {
         transform.position = target.position + offset;
+
+        FadeOutwall();
+
+
     }
+
+
+    // 카메라에서 레이를 쏴 건물을 검출하여 해당 건물을 투명화 시킴 ( 미구현 )
+    private void FadeOutwall()
+    {
+        
     
+    }
 }
